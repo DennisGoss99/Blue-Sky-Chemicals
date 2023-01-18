cd8585e67e45003e3876ec7eebd67131698ce5cb
DennisGoss99 <dennisgossler98@gmail.com>
2023-01-18 23:34:00 +0100

    add pin layout and website example

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,53 @@
 #include <Arduino.h>
+#include "pinLayoutESP32.cpp"
+
 
 void setup() {
   Serial.begin (115200);
   pinMode(2, OUTPUT);
   digitalWrite(2, LOW);
+
+  pinMode(D13, OUTPUT);
+  digitalWrite(D13, LOW);
+
+  pinMode(D4, INPUT);
+
   Serial.println ("Start");
+
+
+  ledcSetup(0, 5000, 8);
+  ledcAttachPin(D13, 0);
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  Serial.println ("high");
-  delay(500);
-  digitalWrite(2, LOW);
-  Serial.println ("low");
-  delay(500);
+
+  float potValue = (analogRead(D14) / 4095.0 -1) * -1;
+  Serial.println(potValue);
+  delay(50);
+
+  // for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the LED brightness with PWM
+  ledcWrite(0, potValue * 255);
+  //   delay(15);
+  // }
+
+  // // decrease the LED brightness
+  // for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+  //   // changing the LED brightness with PWM
+  //   ledcWrite(0, dutyCycle);   
+  //   delay(15);
+  // }
+
+
+  // if(digitalRead(D4)){
+  //   digitalWrite(D13, HIGH);
+  // }else{
+  //   digitalWrite(D13, LOW);
+  // }
+
+  // Serial.println ("high");
+  // delay(500);
+
+  // Serial.println ("low");
+  // delay(500);
 }
---
+++ src/pinLayoutESP32.cpp
@@ -0,0 +1,35 @@
+//DEBO JT ESP32 NodeMCU ESP32 WiFi- und Bluetooth-Modul
+
+#define OnBoardLED 2
+
+#define D0 0
+#define TX0_DONTUSE 1
+#define D2_DANGER 2
+#define RX0_DONTUSE 3
+#define D4 4
+#define D5_DANGER 5
+#define D6_FlashMemory 6
+#define D7_FlashMemory 7
+#define D8_FlashMemory 8
+#define D9_FlashMemory 9
+#define D10_FlashMemory 10
+#define D11_FlashMemory 11
+#define D12_DANGER 12
+#define D13 13
+#define D14 14
+#define D15_DANGER 15
+#define RX2 16
+#define TX2 17
+#define D18 18
+#define D19 19
+#define D21 21
+#define D23 23
+#define D25 25
+#define D26 26
+#define D27 27
+#define D32 32
+#define D33 33
+#define D34_IN 34
+#define D35_IN 35
+#define VP_IN 36
+#define VN_IN 39
